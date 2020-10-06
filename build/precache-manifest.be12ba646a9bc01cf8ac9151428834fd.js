21fecdfe225a58b158b6ad33f7e8278c757877bd
Barrington <barrven@users.noreply.github.com>
2020-10-05 21:48:30 -0400

    Added material

---
+++ build/precache-manifest.be12ba646a9bc01cf8ac9151428834fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce3c91bc89e1ce0e45ea",
+    "url": "/static/css/main.2a6ec1dd.chunk.css"
+  },
+  {
+    "revision": "ce3c91bc89e1ce0e45ea",
+    "url": "/static/js/main.ce3c91bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "45656d1b16d14ef27ae80342e801a55b",
+    "url": "/index.html"
+  }
+];
